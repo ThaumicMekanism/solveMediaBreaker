473886cc9a2b3c9ef376b77cc16576d1d3b5c9fc
Douglas Graham <dg268@kent.ac.uk>
2015-02-09 21:22:20 +0000

    Starting to build out the simpler solvers: the ones that can be done by simple recognition and a dictionary attack.  Patters done and general knowledge started.

--- solveImage.py
+++ solveImage.py
@@ -1,28 +1,46 @@
 #!/usr/bin/python3
 
-from classifyImage import getImageType, blackAndWhiteImage
+from classifyImage import getImageType, blackAndWhiteImage, getInstructionsFromImage
 import pyocr, pyocr.builders
 from PIL import Image
 import numpy
 import sys
+	
+tool = pyocr.get_available_tools()[0]
 
 def solveGeneralknowledge(image):
-	print("Solving general knowledge problem")
+	imageText = tool.image_to_string(image, lang='eng', builder=pyocr.builders.TextBuilder()).lower()
+	if 'liberty' in imageText:
+		return "France"
+	if 'impressionist' in imageText:
+		return 'Edgar Degas'
+	else:
+		return imageText
 
+# This solves all known Next Line challenges, there may very well be more.
 def solveNextline(image):
-	print("Solving next line problem")
+	inst = getInstructionsFromImage(blackAndWhiteImage(image)).lower()
+	print(inst)
+	if 'the n' in inst:
+		return "violets are blue"
+	elif 'finish' in inst:
+		return 'it tolls for thee'
+	elif 'robert' in inst:
+		return 'I took the one less travelled by'
+	else:
+		return None
 
 def solvePatterns(image):
-	tool = pyocr.get_available_tools()[0]
-	print(tool)
 	# Probably need to do some dynamic monochrome-ing here, or some more complex processing to make characters monochrome
 	# Print image numerical data greyscale for testing
-	#data = numpy.array(image.convert('L'))
+	data = numpy.array(image.convert('L')).flatten()
+	print(int(data.mean()))
 	#for x in data:
 	#	print(x)
 	# Save image as greyscale for testing
-	#image.convert('L').save('test1.png')
-	bw = image.convert('L').point( lambda x: 0 if x < 200 else 255)
+	image.convert('L').save('test1.png')
+	bw = image.convert('L').point( lambda x: 0 if x < int(data.mean()) else 255)
+	bw.save('test2.png')
 	# So... for some reason this isn't working... at all.  Returning nothing.  What the shit?!
 	# The 200 constant above will probably need to change to some dynamic value as well.
 	# These lines have only been tested with the images from Martha Stewart, probably not good with
@@ -30,7 +48,7 @@ def solvePatterns(image):
 	return tool.image_to_string(bw, lang='eng', builder=pyocr.builders.TextBuilder())
 
 def solvePleaseenter(image):
-	print("Solving Please enter problem")
+	print("Solving please enter problem")
 
 def solvePleasepick(image):
 	print("Solving please pick problem")
