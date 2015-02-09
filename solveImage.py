5535797bf79c289197e880eb92314da146425003
Douglas Graham <dg268@kent.ac.uk>
2015-02-09 23:44:42 +0000

    Altering comments for accuracy

--- solveImage.py
+++ solveImage.py
@@ -65,17 +65,15 @@ def solvePatterns(image):
 	# Probably need to do some dynamic monochrome-ing here, or some more complex processing to make characters monochrome
 	# Print image numerical data greyscale for testing
 	data = numpy.array(image.convert('L')).flatten()
-	print(int(data.mean()))
+	#print(int(data.mean()))
 	#for x in data:
 	#	print(x)
 	# Save image as greyscale for testing
 	image.convert('L').save('test1.png')
+	# This produces a decent image for the cases I've looked at so far but the OCR is still not returning anything useful.
+	# This is going to be more complicated than I thought.
 	bw = image.convert('L').point( lambda x: 0 if x < int(data.mean()) else 255)
 	bw.save('test2.png')
-	# So... for some reason this isn't working... at all.  Returning nothing.  What the shit?!
-	# The 200 constant above will probably need to change to some dynamic value as well.
-	# These lines have only been tested with the images from Martha Stewart, probably not good with
-	# other images anyway.
 	return tool.image_to_string(bw, lang='eng', builder=pyocr.builders.TextBuilder())
 
 def solvePleaseenter(image):
