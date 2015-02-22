d0295776a057993b38c3074902beee7e45061928
Douglas Graham <dg268@kent.ac.uk>
2015-02-22 21:37:57 +0000

    Abstracted some functionality (OCR and removing to-left instructions).  Also added the bulk of the code to solve pattern-based captchas. 84.9% success on samples so far.

--- solveImage.py
+++ solveImage.py
@@ -12,7 +12,7 @@ tool = pyocr.get_available_tools()[0]
 def solveGeneralknowledge(image):
 	# Get the text from the image, this should be enough to get the question from the image since they're unobfuscated,
 	# also make it lower case and all one line.
-	imageText = tool.image_to_string(image, lang='eng', builder=pyocr.builders.TextBuilder()).lower().replace('\n', ' ')
+	imageText = doOcr(image).replace('\n', ' ')
 	if ('liberty' in imageText) or ('country presented' in imageText):
 		return "France"
 	elif 'impressionist' in imageText:
@@ -71,22 +71,38 @@ def solveNextline(image):
 		print(inst)
 		return None
 
-# This needs much work.  Only need to recognise one character from the challenge but it needs to be accurate all the time.
+def cropOutInstructions(image):
+	return image.crop((0, 17, image.size[0], image.size[1]))
+
+def doOcr(image):
+	return tool.image_to_string(image, lang='eng', builder=pyocr.builders.TextBuilder()).lower()
+
+# 84.9% success rate on the challenges I have.
 def solvePatterns(image):
-	# Probably need to do some dynamic monochrome-ing here, or some more complex processing to make characters monochrome
-	# Print image numerical data greyscale for testing
-	data = numpy.array(image.convert('L')).flatten()
-	#print(int(data.mean()))
-	#for x in data:
-	#	print(x)
-	# Save image as greyscale for testing
-	#image.convert('L').save('test1.png')
-	# This produces a decent image for the cases I've looked at so far but the OCR is still not returning anything useful.
-	# This is going to be more complicated than I thought.
-	bw = image.convert('L').point( lambda x: 0 if x < int(data.mean()) else 255)
-	#bw.save('test2.png')
-	print(tool.image_to_string(bw, lang='eng', builder=pyocr.builders.TextBuilder()))
-	return None
+	# Convert the image to greyscale
+	im = image.convert('L')
+	data = numpy.array(im).flatten()
+	# Get all the UNIQUE values in our numerical data
+	vals = numpy.sort(list(set(data)))
+	im = cropOutInstructions(im)
+	# Here using the mean of all the unique data values rather than the mean of all values in general
+	bw = im.point( lambda x: 0 if x < int(vals.mean()) else 255)
+	# Memo to self: resizing here makes results worse.
+	imageText = doOcr(bw)
+	if '1' in imageText or '2' in imageText or '3' in imageText or '4' in imageText:
+		#bw.save(image.filename.split('.')[0] + '_5_.png')
+		return '5'
+	elif 'a' in imageText or 'b' in imageText or 'c' in imageText or 'd' in imageText:
+		#bw.save(image.filename.split('.')[0] + '_E_.png')
+		return 'e'
+	else:
+		# For some reason some images, even clear ones, do not produce anything via straight OCR.
+		# Might be worth putting something less conventional in at this point later.
+		# Although these present such a small proportion of challenges it's probably not worth it.
+		print(image.filename, imageText)
+		# Save a copy of the image so far; debug uses.
+		#bw.save(image.filename.split('.')[0] + '_fail_.png')
+		return None
 
 def solvePleaseenter(image):
 	print("Solving please enter problem")
